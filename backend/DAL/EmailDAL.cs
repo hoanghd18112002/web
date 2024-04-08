cdbcbc32c6acdf8c3a1392d4888c6f78c4a147e7
hoanghd1811 <hoanghd18112002@gmail.com>
2024-04-08 09:28:48 +0700

    Signed-off-by: hoanghd1811 <hoanghd18112002@gmail.com> 123

--- backend/DAL/EmailDAL.cs
+++ backend/DAL/EmailDAL.cs
@@ -19,7 +19,7 @@ namespace DAL
         public void SendConfirmationEmail(string toEmail, string confirmationLink, string token, string Ten)
         {
             var message = new MimeMessage();
-            message.From.Add(new MailboxAddress(_mailSettings.SenderName, _mailSettings.SenderEmail));
+            message.From.Add(new MailboxAddress(Ten + _mailSettings.SenderName, _mailSettings.SenderEmail));
             message.To.Add(new MailboxAddress("", toEmail));
             message.Subject = "Xác nhận email đăng ký " + Ten;
 
@@ -36,30 +36,10 @@ namespace DAL
             }
         }
 
-        public void SendOrderEmail(string toEmail, string confirmationLink, string Ten, int ID)
-        {
-            var message = new MimeMessage();
-            message.From.Add(new MailboxAddress(_mailSettings.SenderName, _mailSettings.SenderEmail));
-            message.To.Add(new MailboxAddress("", toEmail));
-            message.Subject = "Thông tin đơn hàng";
-
-            var bodyBuilder = new BodyBuilder();
-            bodyBuilder.HtmlBody = $"Cảm ơn bạn đã tin tưởng đặt hàng {Ten},<br/><br/>Mã đơn hàng của bạn là: {ID}<br/><br/>Bạn có thể tra cứu đơn hàng của bạn <a href='{confirmationLink}?id={ID}'>Tại đây</a>";
-            message.Body = bodyBuilder.ToMessageBody();
-
-            using (var client = new SmtpClient())
-            {
-                client.Connect(_mailSettings.Server, _mailSettings.Port, false);
-                client.Authenticate(_mailSettings.SenderEmail, _mailSettings.Password);
-                client.Send(message);
-                client.Disconnect(true);
-            }
-        }
-
         public void SendOrderEmail(string toEmail, string confirmationLink, string Ten, string HoTen, string DiaChi, string SDT, int? ID, List<ChiTietDonHangModel> model)
         {
             var message = new MimeMessage();
-            message.From.Add(new MailboxAddress(_mailSettings.SenderName, _mailSettings.SenderEmail));
+            message.From.Add(new MailboxAddress(Ten + _mailSettings.SenderName, _mailSettings.SenderEmail));
             message.To.Add(new MailboxAddress("", toEmail));
             message.Subject = "Thông tin đơn hàng đặt tại " + Ten;
 
