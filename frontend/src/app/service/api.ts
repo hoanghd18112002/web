a664c3985ae9e89779963a6ae81a9500faaad0da
hoanghd1811 <hoanghd18112002@gmail.com>
2024-05-11 22:30:26 +0700

    22:30 11/5/2024
    Signed-off-by: hoanghd1811 <hoanghd18112002@gmail.com>

--- frontend/src/app/service/api.ts
+++ frontend/src/app/service/api.ts
@@ -5,4 +5,4 @@ export let API_BASE_URL = 'http://localhost:5226/api';
 // export let API_BASE_URL = 'https://localhost:7029/api';
 
 //Deloy
-// export let API_BASE_URL = 'http://103.67.163.152:5226/api';
+// export let API_BASE_URL = 'http://api';
